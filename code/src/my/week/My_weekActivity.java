630c6de3602e350db7c951b119b23211205755ec
Danilo Pena <danilo@dca.ufrn.br>
2018-12-22 22:27:39 -0300

    Added old version

---
+++ code/src/my/week/My_weekActivity.java
@@ -0,0 +1,13 @@
+package my.week;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class My_weekActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
